e9aadd8b07d357f893380cbc54630123e061c7ea
mikezks <23002661+mikezks@users.noreply.github.com>
2024-01-23 11:26:02 +0100

    feat: flight typeahead

--- libs/tickets/feature-booking/src/lib/flight-typeahead/flight-typeahead.component.ts
+++ libs/tickets/feature-booking/src/lib/flight-typeahead/flight-typeahead.component.ts
@@ -1,28 +1,48 @@
-import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Subscription, share, tap, timer } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Flight, FlightService } from '@flight-demo/tickets/domain';
+import { Observable, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'tickets-flight-typeahead',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './flight-typeahead.component.html',
   styleUrl: './flight-typeahead.component.scss',
 })
-export class FlightTypeaheadComponent implements OnDestroy {
-  timer$ = timer(0, 2_000).pipe(
-    tap(value => console.log('Producer', value)),
-    share()
-  );
-  subscription = new Subscription();
+export class FlightTypeaheadComponent {
+  private flightService = inject(FlightService);
 
-  constructor() {
-    this.subscription.add(
-      this.timer$.subscribe(console.log)
+  protected control = new FormControl('', { nonNullable: true });
+  protected flights$ = this.initTypeaheadStream();
+  protected loading = false;
+
+  private initTypeaheadStream(): Observable<Flight[]> {
+    /**
+     * Stream 1: From Control User Input
+     *  - Trigger
+     *  - State Provider: City
+     */
+    return this.control.valueChanges.pipe(
+      // Filtering START
+      filter(city => city.length > 2),
+      debounceTime(300),
+      distinctUntilChanged(),
+      // Filtering END
+      // Side-Effect: Loading State
+      tap(() => this.loading = true),
+      /**
+       * Stream 2: Load Method HTTP Backend API Call
+       *  - State Provider: Flights
+       */
+      switchMap(city => this.load(city)),
+      // Side-Effect: Loading State
+      tap(() => this.loading = false)
     );
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  private load(city: string): Observable<Flight[]> {
+    return this.flightService.find(city, '');
   }
 }
