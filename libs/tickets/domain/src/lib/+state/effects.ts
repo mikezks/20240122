e8d0c7ba900db344d8e0285142219f9195afdde0
mikezks <23002661+mikezks@users.noreply.github.com>
2024-01-24 10:40:22 +0100

    feat: effect operator chain

--- libs/tickets/domain/src/lib/+state/effects.ts
+++ libs/tickets/domain/src/lib/+state/effects.ts
@@ -10,12 +10,26 @@ export class TicketEffects {
   private actions$ = inject(Actions);
 
   loadFlights = createEffect(
+    /**
+     * Stream 1: Dispatched Actions
+     *  - Trigger
+     *  - State Provider: From, To
+     */
     () => this.actions$.pipe(
+      // Filtering
       ofType(ticketActions.flightsLoad),
+      /**
+       * Stream 2: Load Method HTTP Backend API Call
+       *  - State Provider: Flights
+       */
+      // Access additional State
+      // withLatestFrom() or
+      // concatLatestFrom()
       switchMap(action => this.flightService.find(
         action.from,
         action.to
       )),
+      // Transformation: Flights to Action
       map(flights => ticketActions.flightsLoaded({ flights }))
     )
   );
